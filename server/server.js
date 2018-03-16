510b313a53c0b6b57b61b3f76d2358eeb996236a
Chimp <jesseducharme2@gmail.com>
2018-03-15 22:39:24 -0400

    Forgot a stupid dot

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
-const publicPath = path.join(__dirname + '/../public');
 const express = require('express'); 
-const port = process_env.PORT || 3000 ; 
+const publicPath = path.join(__dirname + '/../public');
+const port = process.env.PORT || 3000 ; 
 
 var app = express() ; 
 app.use(express.static(publicPath));
